f0c0b27f79f2c253122d0379831aa784518f7cb6
katelynwang <wang.ke2@husky.neu.edu>
2018-02-21 16:01:00 -0500

    add view patient reprots, minor changes on menu drawer

--- lib/LabWorkPart/ViewPatientReports.dart
+++ lib/LabWorkPart/ViewPatientReports.dart
@@ -5,6 +5,7 @@ import '../Material/MenuDrawer.dart';
 class ViewPatientReports extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+//    Decoration rowDeco = new Decoration();
     return new Scaffold(
       endDrawer: new Drawer(
         child: new MenuDrawer(),
@@ -19,17 +20,64 @@ class ViewPatientReports extends StatelessWidget {
         ),
       ),
 
-      body: new Column(
-        children: <Widget>[
-          new Container(
-              alignment: Alignment.center,
-              child: new Column(
+      body: new Container(
+        padding: new EdgeInsets.only(top: 20.0, left: 2.0),
+        child: new Table( //TODO: horizontal view
+          columnWidths: const <int, TableColumnWidth> {
+            0: const FixedColumnWidth(190.0),
+            1: const FixedColumnWidth(100.0),
+            2: const FixedColumnWidth(70.0),
+          },
+          border: new TableBorder(
+              top: new BorderSide(color: const Color(0x42000000), width: 1.0),
+              bottom: new BorderSide(color: const Color(0x42000000), width: 1.0),
+              left: new BorderSide(color: const Color(0x42000000), width: 1.0),
+              right: new BorderSide(color: const Color(0x42000000), width: 1.0),
+              verticalInside: new BorderSide(width: 1.0, color: const Color(0x42000000)),    //Colors.black45
+              horizontalInside: new BorderSide(width: 1.0, color: const Color(0x61000000))
+          ),
+          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+
+          children: <TableRow>[
+            new TableRow(
                 children: <Widget>[
-                  new Text("To be continued")
+                  new TableCell(child: new Text('  Ordered Tests', style: new TextStyle(height: 1.8))),
+                  new TableCell(child: new Text('Date', textAlign: TextAlign.center, style: new TextStyle(height: 1.8))),
+                  new TableCell(child: new Text('Time', textAlign: TextAlign.center, style: new TextStyle(height: 1.8)))
                 ],
-              )
-          )
-        ],
+                decoration: new BoxDecoration(color: const Color(0xFFE0E0E0))
+            ),
+            new TableRow(
+                children: <Widget>[
+                  new TableCell(child: new Text('  CBC with Differential', style: new TextStyle(height: 1.5))),
+                  new TableCell(child: new Text('02/21/2018', textAlign: TextAlign.center, style: new TextStyle(height: 1.5))),
+                  new TableCell(child: new Text('13:33', textAlign: TextAlign.center, style: new TextStyle(height: 1.5)))
+                ]
+            ),
+            new TableRow(
+                children: <Widget>[
+                  new TableCell(child: new Text('  CRP (C-Reactive Protein)', style: new TextStyle(height: 1.5))),
+                  new TableCell(child: new Text('02/21/2018', textAlign: TextAlign.center, style: new TextStyle(height: 1.5))),
+                  new TableCell(child: new Text('13:33', textAlign: TextAlign.center, style: new TextStyle(height: 1.5)))
+                ]
+            ),
+            new TableRow(
+                children: <Widget>[
+                  new TableCell(child: new Text('  Clotting Studies', style: new TextStyle(height: 1.5))),
+                  new TableCell(child: new Text('02/21/2018', textAlign: TextAlign.center, style: new TextStyle(height: 1.5))),
+                  new TableCell(child: new Text('13:33', textAlign: TextAlign.center, style: new TextStyle(height: 1.5)))
+                ]
+            ),
+            new TableRow(
+                children: <Widget>[
+                  new TableCell(child: new Text('  Electrolytes', style: new TextStyle(height: 1.5))),
+                  new TableCell(child: new Text('02/21/2018', textAlign: TextAlign.center, style: new TextStyle(height: 1.5))),
+                  new TableCell(child: new Text('13:33', textAlign: TextAlign.center, style: new TextStyle(height: 1.5)))
+                ]
+            )
+          ]
+
+        ),
       ),
     );
   }
