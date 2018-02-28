71f0561bd4218f849de6ed23780383c7ce1b49f4
katelynwang <wang.ke2@husky.neu.edu>
2018-02-28 14:19:42 -0500

    Finished patient info validation

--- lib/PssatForm/PatientInfo.dart
+++ lib/PssatForm/PatientInfo.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -12,7 +10,7 @@ class PatientData {
   String estGa = '';
   String growth = '';
   String admitted = '';
-  String resuscitationAtBirth = '';
+  String resuscitation = '';
   String referral = '';
   String resuscitationMeds = '';        //textformfield
   String otherMeds = '';                //textformfield
@@ -38,7 +36,7 @@ class PatientInfoState extends State<PatientInfo> {
   String _ddEstGa;
   String _ddGrowth;
   String _ddAdmitted;
-  String _ddResAtBirth;
+  String _ddResuscitation;
   String _ddReferral;
   String _dd1Min;
   String _dd5Min;
@@ -51,18 +49,23 @@ class PatientInfoState extends State<PatientInfo> {
   final List<String> _estGa = <String>['GA 1', 'GA 2', 'GA 3', 'GA 4', 'GA 5', 'GA 6',];
   final List<String> _growth = <String>['Growth 1', 'Growth 2', 'Growth 3', 'Growth 4', 'Growth 5', 'Growth 6'];
   final List<String> _admitted = <String>['Central', 'East', 'West', 'Other'];
-  final List<String> _resAtBirth = <String>['True', 'False'];
+  final List<String> _resuscitation = <String>['True', 'False'];
   final List<String> _referral = <String>['Referral 1', 'Referral 2', 'Referral 3', 'Referral 4', 'Referral 5', 'Referral 6'];
-  final List<String> _1Min = <String>['Apgar 1', 'Apgar 2', 'Apgar 3', 'Apgar 4', 'Apgar 5', 'Apgar 6'];
-  final List<String> _5Min = <String>['Apgar 1', 'Apgar 2', 'Apgar 3', 'Apgar 4', 'Apgar 5', 'Apgar 6'];
-  final List<String> _10Min = <String>['Apgar 1', 'Apgar 2', 'Apgar 3', 'Apgar 4', 'Apgar 5', 'Apgar 6'];
-  final List<String> _15Min = <String>['Apgar 1', 'Apgar 2', 'Apgar 3', 'Apgar 4', 'Apgar 5', 'Apgar 6'];
-  final List<String> _20Min = <String>['Apgar 1', 'Apgar 2', 'Apgar 3', 'Apgar 4', 'Apgar 5', 'Apgar 6'];
+  final List<String> _at1Min = <String>['Apgar 1', 'Apgar 2', 'Apgar 3', 'Apgar 4', 'Apgar 5', 'Apgar 6'];
+  final List<String> _at5Min = <String>['Apgar 1', 'Apgar 2', 'Apgar 3', 'Apgar 4', 'Apgar 5', 'Apgar 6'];
+  final List<String> _at10Min = <String>['Apgar 1', 'Apgar 2', 'Apgar 3', 'Apgar 4', 'Apgar 5', 'Apgar 6'];
+  final List<String> _at15Min = <String>['Apgar 1', 'Apgar 2', 'Apgar 3', 'Apgar 4', 'Apgar 5', 'Apgar 6'];
+  final List<String> _at20Min = <String>['Apgar 1', 'Apgar 2', 'Apgar 3', 'Apgar 4', 'Apgar 5', 'Apgar 6'];
 
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  final _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   bool _autoValidate = false;
 
   String _validateBirthWeight(String value) {
+    if (value.isEmpty)
+      return 'Birth weight is required';
+
     final RegExp numExp = new RegExp(r'(?!^0*$)(?!^0*\.0*$)^\d{1,2}(\.\d{1,2})?$');
 
     if (!numExp.hasMatch(value))
@@ -71,7 +74,10 @@ class PatientInfoState extends State<PatientInfo> {
   }
 
   String _validateBirthOrder(String value) {
-    final RegExp numExp = new RegExp(r'^[0-9]$');
+    if (value.isEmpty)
+      return 'Birth order is required';
+
+    final RegExp numExp = new RegExp(r'^[1-9]$');
 
     if (!numExp.hasMatch(value))
       return 'Only allow 1 digit';
@@ -80,16 +86,18 @@ class PatientInfoState extends State<PatientInfo> {
 
   void _handleSubmitted() {
     final FormState form = _formKey.currentState;
-    form.save();
 
-    if (patient.birthWeight == '' || patient.birthOrder == '' || patient.gender == '' || patient.estGa == '' ||
-        patient.growth == '' || patient.admitted == '' || patient.resuscitationAtBirth == '' || patient.referral == '') {
+    if (!form.validate()){
+      _autoValidate = true; // Start validating on every change.
+      final snackbar = new SnackBar(content: new Text('Please re-enter field that marked in red'),);
+      _scaffoldKey.currentState.showSnackBar(snackbar);
+
+    } else if (patient.gender == '' || patient.estGa == '' ||patient.growth == '' ||
+        patient.admitted == '' || patient.resuscitation == '' || patient.referral == '') {
       showDialog(
           context: context,
           child: new AlertDialog(
-            content: new Text(
-                'Please complete all reqired fields! ..../${patient
-                    .birthWeight}..../${patient.birthOrder}....'),
+            content: new Text('Please enter all required fields! '),
             actions: <Widget>[
               new RaisedButton(
                   child: new Text("OK"),
@@ -99,20 +107,7 @@ class PatientInfoState extends State<PatientInfo> {
             ],
           )
       );
-    } else if (!form.validate()){
-      _autoValidate = true; // Start validating on every change.
-      showDialog(
-          context: context,
-          child: new AlertDialog(
-            content: new Text('Illegal input!'),
-            actions: <Widget>[
-              new RaisedButton(
-                  child: new Text("OK"),
-                  onPressed: () {Navigator.of(context).pop(false);})
-            ],
-          )
-      );
-    } else{
+    } else {
       form.save();
       showDialog(
           context: context,
@@ -130,7 +125,7 @@ class PatientInfoState extends State<PatientInfo> {
                     showDialog(
                         context: context,
                         child: new AlertDialog(
-                          content: new Text('New patient added successfully!'),
+                          content: new Text('Patient information saved successfully!'),
                           actions: <Widget>[
                             new RaisedButton(
                                 child: new Text("OK"),
@@ -152,8 +147,9 @@ class PatientInfoState extends State<PatientInfo> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      endDrawer: new Drawer(child: new MenuDrawer(),),
+      key: _scaffoldKey,
 
+      endDrawer: new Drawer(child: new MenuDrawer(),),
       appBar: new AppBar(
         title: new Text(
             "Patient Information", style: new TextStyle(color: Colors.black),
@@ -166,31 +162,27 @@ class PatientInfoState extends State<PatientInfo> {
           autovalidate: _autoValidate,
 
           child: new ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
+          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
           children: <Widget>[
-            new Container(
-              child: new Text('Patient Information', style: Theme.of(context).textTheme.button),
-              alignment: Alignment.centerLeft,
-              padding: const EdgeInsets.only(bottom: 10.0),
-            ),
 
             // Birth weight
             new TextFormField(
-              initialValue: patient.birthWeight,
+//              initialValue: patient.birthWeight,
               decoration: const InputDecoration(
                 labelText: 'Birth Weight *',
-                hintText: 'Please enter numbers',
+                helperText: 'Please enter numbers',
               ),
+              keyboardType: TextInputType.number,
               onSaved: (String value) {patient.birthWeight = value;},
               validator: _validateBirthWeight,
             ),
 
             // Birth order
             new TextFormField(
-              initialValue: patient.birthOrder,
+//              initialValue: patient.birthOrder,
               decoration: const InputDecoration(
                 labelText: 'Birth Order *',
-                hintText: 'Please enter digits only',
+                helperText: 'Please enter 1 digit only',
               ),
               keyboardType: TextInputType.number,
               onSaved: (String value) {patient.birthOrder = value;},
@@ -309,14 +301,14 @@ class PatientInfoState extends State<PatientInfo> {
                   child: new Container(
                     padding: new EdgeInsets.only(top: 10.0),
                     child: new DropdownButton<String>(
-                      value: _ddResAtBirth,
+                      value: _ddResuscitation,
                       onChanged: (String newValue) {
                         setState(() {
-                          _ddResAtBirth = newValue;
-                          patient.resuscitationAtBirth = _ddResAtBirth;
+                          _ddResuscitation = newValue;
+                          patient.resuscitation = _ddResuscitation;
                         });
                       },
-                      items: _resAtBirth.map((String value) {
+                      items: _resuscitation.map((String value) {
                         return new DropdownMenuItem<String>(
                           value: value,
                           child: new Text(value),
@@ -379,7 +371,7 @@ class PatientInfoState extends State<PatientInfo> {
 
             // Apgar
             new Container(
-                padding: new EdgeInsets.symmetric(vertical: 50.0),
+                padding: new EdgeInsets.only(top: 50.0),
                 child: new Column(
                   children: <Widget>[
                     new Container(
@@ -400,7 +392,7 @@ class PatientInfoState extends State<PatientInfo> {
                                   patient.at1min = _dd1Min;
                                 });
                               },
-                              items: _1Min.map((String value) {
+                              items: _at1Min.map((String value) {
                                 return new DropdownMenuItem<String>(
                                   value: value,
                                   child: new Text(value),
@@ -422,7 +414,7 @@ class PatientInfoState extends State<PatientInfo> {
                                   patient.at5min = _dd5Min;
                                 });
                               },
-                              items: _5Min.map((String value) {
+                              items: _at5Min.map((String value) {
                                 return new DropdownMenuItem<String>(
                                   value: value,
                                   child: new Text(value),
@@ -449,7 +441,7 @@ class PatientInfoState extends State<PatientInfo> {
                                   patient.at10min = _dd10Min;
                                 });
                               },
-                              items: _10Min.map((String value) {
+                              items: _at10Min.map((String value) {
                                 return new DropdownMenuItem<String>(
                                   value: value,
                                   child: new Text(value),
@@ -471,7 +463,7 @@ class PatientInfoState extends State<PatientInfo> {
                                   patient.at15min = _dd15Min;
                                 });
                               },
-                              items: _15Min.map((String value) {
+                              items: _at15Min.map((String value) {
                                 return new DropdownMenuItem<String>(
                                   value: value,
                                   child: new Text(value),
@@ -498,7 +490,7 @@ class PatientInfoState extends State<PatientInfo> {
                                   patient.at20min = _dd20Min;
                                 });
                               },
-                              items: _20Min.map((String value) {
+                              items: _at20Min.map((String value) {
                                 return new DropdownMenuItem<String>(
                                   value: value,
                                   child: new Text(value),
@@ -517,11 +509,13 @@ class PatientInfoState extends State<PatientInfo> {
             ),
 
             // Hint
-            new Container(child: new Text('* indicates required field', style: Theme.of(context).textTheme.caption),),
+            new Container(
+                padding: new EdgeInsets.only(top: 50.0),
+                child: new Text('* indicates required field', style: Theme.of(context).textTheme.caption),),
 
             // Submit button
             new Container(
-              padding: const EdgeInsets.all(50.0),
+              padding: const EdgeInsets.only(top: 50.0),
               alignment: Alignment.center,
               child: new RaisedButton(
                 child: const Text('Submit'),
@@ -538,10 +532,10 @@ class PatientInfoState extends State<PatientInfo> {
 
 }
 
-void main() =>
-    runApp(
-        new MaterialApp(
-          title: 'Patient Information',
-          home: new PatientInfo(),
-        )
-    );
+//void main() =>
+//    runApp(
+//        new MaterialApp(
+//          title: 'Patient Information',
+//          home: new PatientInfo(),
+//        )
+//    );
